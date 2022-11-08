bef5228659eea7b51eaaab231bf8b1fd2c7d74e5
Htin Lynn <htinlynn@webquest.io>
2022-11-08 13:30:39 +0800

    feat: upload api

--- pages/api/upload.ts
+++ pages/api/upload.ts
@@ -2,22 +2,13 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import * as fs from 'fs';
 import aws from "aws-sdk";
 import formidable from "formidable"
+import { now } from "moment";
 
 export const config = {
     api: {
         bodyParser: false,
     }
 };
-
-interface FileWithPath extends File {
-    path: any
-}
-
-interface FileWithName extends File {
-    name: string
-}
-
-type ProcessedFiles = Array<[string, File]>;
 
 const s3 = new aws.S3({
     endpoint: "sgp1.digitaloceanspaces.com",
@@ -37,16 +28,24 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 
     //
     form.parse(req, async function (err, fields, files) {
-        return await saveFile(files.profile);
+        if (files.profile) {
+            return await saveFile(files.profile);
+        } else if(files.examResult) {
+            return await saveExamResult(files.examResult);
+        }   else if(files.certificates) {
+            return await saveCertificates(files.certificates);
+        }   else if(files.transcripts) {
+            return await saveTranscripts(files.transcripts);
+        }
     });
 
     const saveFile = async (file: any) => {
 
         const f = Array.isArray(file) ? file.map((f) => f) : file;
-
+        const fileName = now().valueOf()
         const params = {
             Bucket: "genius", // The path to the directory you want to upload the object to, starting with your Space name.
-            Key: "users/" + f.originalFilename, // Object key, referenced whenever you want to access this file later.
+            Key: "users/" + fileName + '.' + f.originalFilename.split('.').pop(), // Object key, referenced whenever you want to access this file later.
             Body: fs.readFileSync(f.filepath), // The object's contents. This variable is an object, not a string.
             ACL: "public-read", // Defines ACL permissions, such as private or public.
             Metadata: { // Defines metadata tags.
@@ -64,7 +63,107 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
                 return res.status(500).json({url: ''});
             }
             return res.status(200).json({
-                url: data.Location
+                url: data.Location,
+                fileName: f.originalFilename
+            });
+        });
+
+
+
+    };
+
+    const saveExamResult = async (file: any) => {
+
+        const f = Array.isArray(file) ? file.map((f) => f) : file;
+        const fileName = now().valueOf()
+        const params = {
+            Bucket: "genius", // The path to the directory you want to upload the object to, starting with your Space name.
+            Key: "examResults/" + fileName + '.' + f.originalFilename.split('.').pop(), // Object key, referenced whenever you want to access this file later.
+            Body: fs.readFileSync(f.filepath), // The object's contents. This variable is an object, not a string.
+            ACL: "public-read", // Defines ACL permissions, such as private or public.
+            Metadata: { // Defines metadata tags.
+                "Content-Type": f.mimetype,
+                "Content-Encoding": 'gzip',
+                "x-amz-acl": 'public-read'
+            }
+        };
+
+        //console.log('params', params)
+
+        s3.upload(params).send((err, data) => {
+            if (err) {
+                console.log('err', err);
+                return res.status(500).json({url: ''});
+            }
+            return res.status(200).json({
+                url: data.Location,
+                fileName: f.originalFilename
+            });
+        });
+
+
+
+    };
+
+    const saveCertificates = async (file: any) => {
+
+        const f = Array.isArray(file) ? file.map((f) => f) : file;
+        const fileName = now().valueOf()
+        const params = {
+            Bucket: "genius", // The path to the directory you want to upload the object to, starting with your Space name.
+            Key: "certificates/" + fileName + '.' + f.originalFilename.split('.').pop(), // Object key, referenced whenever you want to access this file later.
+            Body: fs.readFileSync(f.filepath), // The object's contents. This variable is an object, not a string.
+            ACL: "public-read", // Defines ACL permissions, such as private or public.
+            Metadata: { // Defines metadata tags.
+                "Content-Type": f.mimetype,
+                "Content-Encoding": 'gzip',
+                "x-amz-acl": 'public-read'
+            }
+        };
+
+        //console.log('params', params)
+
+        s3.upload(params).send((err, data) => {
+            if (err) {
+                console.log('err', err);
+                return res.status(500).json({url: ''});
+            }
+            return res.status(200).json({
+                url: data.Location,
+                fileName: f.originalFilename
+            });
+        });
+
+
+
+    };
+
+    const saveTranscripts = async (file: any) => {
+
+        const f = Array.isArray(file) ? file.map((f) => f) : file;
+        const fileName = now().valueOf()
+        const params = {
+            Bucket: "genius", // The path to the directory you want to upload the object to, starting with your Space name.
+            Key: "transcripts/" + fileName + '.' + f.originalFilename.split('.').pop(), // Object key, referenced whenever you want to access this file later.
+            Body: fs.readFileSync(f.filepath), // The object's contents. This variable is an object, not a string.
+            ACL: "public-read", // Defines ACL permissions, such as private or public.
+            Metadata: { // Defines metadata tags.
+                "Content-Type": f.mimetype,
+                "Content-Encoding": 'gzip',
+                "x-amz-acl": 'public-read'
+            }
+        };
+
+        //console.log('params', params)
+
+        s3.upload(params).send((err, data) => {
+            if (err) {
+                console.log('err', err);
+                return res.status(500).json({url: ''});
+            }
+            return res.status(200).json({
+                url: data.Location,
+                fileName: f.originalFilename
             });
         });
 
